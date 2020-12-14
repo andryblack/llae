e573d9c30b109c95c01b8d2d2da7560aab212592
andryblack <blackicebox@gmail.com>
2020-12-14 03:38:56 +0300

    fix main

--- data/main-template.lua
+++ data/main-template.lua
@@ -1,34 +1,6 @@
 package.path = 'scripts/?.lua;build/scripts/?.lua'
 
--- local uv = require 'uv'
--- local utils = require 'llae.utils'
+local utils = require 'llae.utils'
+args = utils.parse_args(...)
 
-app = {}
-
-return function( args )
-
-	app.args = args
-
-	-- local cmdargs = utils.parse_args(args)
-	-- local scripts = cmdargs.scripts
-	-- if scripts then
-	-- 	print('use scripts at',scripts)
-	-- 	table.remove(package.searchers,1)
-	-- 	local store_packages = {
-	-- 		'json','llae','uv','ssl'
-	-- 	}
-	-- 	local store = { }
-	-- 	for _,v in ipairs(store_packages) do
-	-- 		store[v] = package.loaded[v]
-	-- 	end
-	-- 	utils.clear_table(package.loaded)
-	-- 	for k,v in pairs(store) do
-	-- 		package.loaded[k]=v
-	-- 	end
-	-- 	package.path = scripts .. '/?.lua' 
-	-- end
-	
-	local m = require( 'main' )
-
-end
-
+require( 'main' )
