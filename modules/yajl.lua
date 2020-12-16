128bd15bbdf6c9289a8131b3149613ffe55b461e
andryblack <blackicebox@gmail.com>
2020-12-16 22:49:09 +0300

    fix yajl module

--- modules/yajl.lua
+++ modules/yajl.lua
@@ -12,6 +12,7 @@ function install()
 	download(url,archive,hash)
 	unpack_tgz(archive)
 	shell([[
+rm -rf ]] .. dir .. [[
 mv lloyd-yajl-]] .. revision .. ' ' .. dir .. [[ || exit 1
 	]])
 	install_files{ 
