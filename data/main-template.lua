36c8bdb23403c98dd2d982eabca9cf1b14c9db1a
andryblack <blackicebox@gmail.com>
2021-02-15 00:30:20 +0300

    emable verbose on builtin

--- data/main-template.lua
+++ data/main-template.lua
@@ -3,4 +3,8 @@ package.path = 'scripts/?.lua;build/scripts/?.lua'
 local utils = require 'llae.utils'
 args = utils.parse_args(...)
 
+if args.verbose then
+	(require 'llae.log').set_verbose(true)
+end
+
 require( 'main' )
