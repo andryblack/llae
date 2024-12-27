e5f5c19cb251621036ceecf1ff2fcadc7e452122
andryblack <blackicebox@gmail.com>
2024-12-27 16:20:06 +0300

    fix win

--- modules/libuv.lua
+++ modules/libuv.lua
@@ -98,6 +98,6 @@ project_main = [[
 	filter "system:linux"
 		links{ 'pthread','dl' }
 	filter "system:windows"
-		links{ 'psapi','user32','advapi32','iphlpapi','userenv','ws2_32','dbghelp' }
+		links{ 'psapi','user32','advapi32','iphlpapi','userenv','ws2_32','dbghelp','ole32' }
 	filter {}
 ]]
