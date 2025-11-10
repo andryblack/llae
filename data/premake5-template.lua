58aee9f537d0c16e76eed6c5065d0543450649ac
andryblack <blackicebox@gmail.com>
2025-11-10 22:10:54 +0300

    use externalincludedirs for deps

--- data/premake5-template.lua
+++ data/premake5-template.lua
@@ -117,7 +117,7 @@ solution '<%= project:name() %>'
 
 
 
-		includedirs {
+		externalincludedirs {
 			<% for _,mod in project:foreach_module() do if mod.includedir then %>
 				'<%= project.get_path(path.join('modules',mod.name),utils.replace_tokens(mod.includedir,mod)) %>',<%  
 				elseif mod.includedirs then
