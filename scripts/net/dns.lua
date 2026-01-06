19e6a226e7bbffd2ca662363b33b709db2b8ef69
andryblack <blackicebox@gmail.com>
2026-01-06 11:28:22 +0300

    dns resolve plugin

--- scripts/net/dns.lua
+++ scripts/net/dns.lua
@@ -1,5 +1,16 @@
 local log = require 'llae.log'
 local uv = require 'llae.uv'
+local class = require 'llae.class'
+
+---@class net.dns.plugin
+local plugin = class(nil,'net.dns.plugin')
+
+---@param host string
+---@return uv.getaddrinfo.item[]?
+---@return string?
+function plugin:resolve(host)
+	return nil,'not implemented'
+end
 
 ---@class net.dnd.ip_entry
 
@@ -11,7 +22,11 @@ local dns = {
 	---@type table<string,net.dns.cache_entry>
 	_resolve_cache = {},
 	---@type table<string,uv.getaddrinfo.item[]>
-	_override = {}
+	_override = {},
+	---@type net.dns.plugin[]
+	_plugins = {},
+
+	plugin = plugin
 }
 
 
@@ -29,8 +44,26 @@ function dns.resolve( host )
 	end
 	log.debug('resolve',host)
 	local ip_list,err = uv.getaddrinfo(host)
+	if not ip_list or not next(ip_list) then
+		for i,v in ipairs(dns._plugins) do
+			---@type string?
+			local perr
+			ip_list,perr = v:resolve(host)
+			if ip_list and next(ip_list) then
+				break
+			end
+			if perr then
+				if not err then
+					err = perr
+				else
+					err = err .. ', ' .. perr
+				end
+			end
+		end
+	end
+
 	if not ip_list then
-		return nil,err
+		return nil,err or 'not found'
 	end
 	if next(ip_list) then
 		dns._resolve_cache[host] = {
@@ -45,6 +78,21 @@ function dns.override(host,addr)
 	local over = dns._override[host] or {}
 	dns._override[host] = over
 	table.insert(over,addr)
+end
+
+---@param plugin net.dns.plugin
+function dns.register_plugin(plugin)
+	table.insert(dns._plugins,plugin)
+end
+
+---@param plugin net.dns.plugin
+function dns.unregister_plugin(plugin)
+	for i,v in ipairs(dns._plugins) do
+		if v == plugin then
+			table.remove(dns._plugins,i)
+			break
+		end
+	end
 end
 
 return dns
