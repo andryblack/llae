86e834940c1def60b417ced41a9830f2aaff3905
andryblack <blackicebox@gmail.com>
2026-09-06 01:21:56 +0300

    RSA public key operation

--- src/crypto/rsa.cpp
+++ src/crypto/rsa.cpp
@@ -1,17 +1,60 @@
 #include "rsa.h"
 #include "crypto.h"
+#include "llae/buffer.h"
+#include "llae/error.h"
+#include "llae/promise.h"
+#include "llae/work.h"
+
 
 META_OBJECT_INFO(crypto::rsa_base,meta::object)
 
 namespace crypto {
+
+	using rsa_base_ptr = common::intrusive_ptr<rsa_base>;
 
 	rsa_base::~rsa_base() {
 		
 	}
 
 	llae::result<> rsa_base::set_padding(int padding,std::optional<mbedtls_md_type_t> hash_id) {
+		if (!m_ctx) {
+			return llae::string_error::create("RSA context not initialized");
+		}
 		auto res = mbedtls_rsa_set_padding(m_ctx,padding,hash_id.has_value() ? hash_id.value() : MBEDTLS_MD_NONE);
 		return make_result(res);
+	}
+
+	llae::result<size_t> rsa_base::get_len() const {
+		if (!m_ctx) {
+			return llae::string_error::create("RSA context not initialized");
+		}
+		return mbedtls_rsa_get_len(m_ctx);
+	}
+
+	llae::result<llae::buffer_base_ptr> rsa_base::sync_public(const llae::buffer_view& buffer) {
+		if (!m_ctx) {
+			return llae::string_error::create("RSA context not initialized");
+		}
+		auto len = mbedtls_rsa_get_len(m_ctx);
+		if (buffer.get_len() != len) {
+			return llae::string_error::create("invalid buffer size");
+		}
+		llae::buffer_ptr result = llae::buffer::alloc(len);
+		auto res = mbedtls_rsa_public(m_ctx,static_cast<const unsigned char*>(buffer.get_base()),static_cast<unsigned char*>(result->get_base()));
+		if (res != 0) {
+			return status_error::create(res);
+		}
+		return llae::buffer_base_ptr(result);
+	}
+
+	llae::result_promise_ptr<llae::buffer_base_ptr> rsa_base::async_public(llae::loop& a,llae::buffer_base_ptr buffer) {
+        if (!buffer) {
+            return llae::result_promise_forward_error<llae::buffer_base_ptr>(llae::string_error::create("need buffer"));
+        }
+		using work_t = llae::function_work<llae::buffer_base_ptr>;
+		return work_t::start(a, [self = rsa_base_ptr(this),lbuffer = std::move(buffer)](){
+			return self->sync_public(*lbuffer);
+		});
 	}
 
 }
--- src/crypto/rsa.h
+++ src/crypto/rsa.h
@@ -3,7 +3,14 @@
 #include "llae-private/mbedtls/rsa.h"
 #include "meta/object.h"
 #include "llae/result.h"
+#include "llae/buffer.h"
+#include "llae/promise.h"
 #include <optional>
+
+
+namespace llae {
+	class loop;
+}
 
 namespace crypto {
 
@@ -19,12 +26,22 @@ namespace crypto {
 	public:
 		~rsa_base();
 
+		/// @luabind
+		llae::result<size_t> get_len() const;
+
 		/// Sets the padding mode for RSA operations.
 		/// @lparam(padding,integer) Padding mode (RSA_PKCS_V15 or RSA_PKCS_V21)
 		/// @lreturn(result,boolean?)
 		/// @lreturn(error,string?)
 		/// @luabind
 		llae::result<> set_padding(int padding,std::optional<mbedtls_md_type_t> hash_id);
+
+		/// Performs an RSA public key operation.
+		/// @luabind
+		llae::result<llae::buffer_base_ptr> sync_public(const llae::buffer_view& buffer);
+		/// Performs an RSA public key operation.
+		/// @luabind(name=public,async=true)
+		llae::result_promise_ptr<llae::buffer_base_ptr> async_public(llae::loop& a,llae::buffer_base_ptr buffer);
 	};
 
 }
